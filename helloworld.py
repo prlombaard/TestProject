2928df5c2b66d02fe17961bdcef42a8e4eae9786
Rudolph Lombaard <prlombaard@gmail.com>
2019-01-19 10:20:08 +0200

    Update helloworld.py
    Adds add function to main function

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,14 @@
 import time
 
 def add(x, y):
-	return a + b
+    return a + b
 
 
 def main():
-	print("Hello World")
+    print("Hello World")
 	print("I'm going to add something")
+	answer = add(3,5)
+	print("Adding 3 + 5 ="+answer)
 	
 if __name__ == '__main__':
 	main()
