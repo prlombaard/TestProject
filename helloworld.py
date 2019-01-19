f0d119e76d65722ce7ff12c7c701e95e81d54566
Rudolph Lombaard <prlombaard@gmail.com>
2019-01-19 10:22:03 +0200

    Update helloworld.py
    Fixed tabs and spacing

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,14 @@
 import time
 
 def add(x, y):
-    return a + b
+	return x + y
 
 
 def main():
-    print("Hello World")
+	print("Hello World")
 	print("I'm going to add something")
 	answer = add(3,5)
-	print("Adding 3 + 5 ="+answer)
+	print("Adding 3 + 5 =",answer)
 	
 if __name__ == '__main__':
 	main()
